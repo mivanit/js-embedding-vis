5167b7ad3c528cbcc7eb8f25b35c2a50387dc832
Michael Ivanitskiy <mivanits@umich.edu>
2025-06-25 23:44:11 -0700

    initial demo

---
+++ get_data.py
@@ -0,0 +1,59 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "numpy",
+#   "scikit-learn",
+# ]
+# ///
+
+from pathlib import Path
+import json
+
+import numpy as np
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+def write_data_jsonl(
+	data_raw: dict,
+	output_path: Path,
+) -> None:
+
+	data_pca: PCA = PCA(n_components=3)
+	_ = data_pca.fit(data_raw["data"])
+	pca_values: np.ndarray = data_pca.transform(data_raw["data"])
+
+	data_transformed: list[dict[str, float | int | str]] = [
+		{
+			# raw data
+			**{
+				f"data.{feat_name}": float(row[i])
+				for i, feat_name in enumerate(data_raw["feature_names"])
+			},
+			# principal components
+			**{
+				f"pc.{i + 1}": float(pc_val)
+				for i, pc_val in enumerate(pca_row)
+			},
+			# target
+			"target_int": int(data_raw["target"][idx]),
+			"target_name": str(
+				data_raw["target_names"][data_raw["target"][idx]]
+			),
+		}
+		for idx, (row, pca_row) in enumerate(zip(data_raw["data"], pca_values))
+	]
+
+	# quick checks
+	assert len(data_transformed) == len(data_raw["data"])
+	assert {"pc.1", "pc.2", "pc.3"} <= data_transformed[0].keys()
+
+	# save to JSON
+	with output_path.open("w") as f:
+		for item in data_transformed:
+			f.write(json.dumps(item) + "\n")
+
+if __name__ == "__main__":
+	write_data_jsonl(
+		data_raw=datasets.load_iris(),
+		output_path=Path("docs/iris_pca.jsonl"),
+	)
--- makefile
+++ makefile
@@ -2,3 +2,8 @@
 .PHONY: bundle
 bundle:
 	uv run bundle.py
+
+.PHONY: demo
+demo: bundle
+	cp bundled/index.html docs/index.html
+	uv run get_data.py
